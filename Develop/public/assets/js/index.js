a3566275b4770ae4cb0903481f336dcf3ed1ed58
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-03-18 15:12:12 -0500

    Added Express package, port for server, express data parsing, two routing paths and start server

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,3 +1,28 @@
+// npm packages
+const express = require('express');
+
+const app = express();
+
+// Set initial port
+const PORT = process.env.PORT || 8080;
+
+// Allow Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routing
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+// Start server
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`)); 
+
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
