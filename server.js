11666d8bfd9288968a47184572b856c12ff5ac9c
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-03-22 23:29:35 -0500

    Moved files around to get static working properly

--- server.js
+++ server.js
@@ -11,26 +11,38 @@ const PORT = process.env.PORT || 8080;
 // Allow Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('Develop/public/'));
+app.use(express.static('./'));
 
 // Routing
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, '/index.html'))
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/notes.html'));
 });
 
+// const readFile = 
+function parse () {
+  let notes = fs.readFileSync(__dirname + '/db.json');
+  let parsed = JSON.parse(notes);
+  return parsed;
+}
 app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, '/Develop/db/db.json'))
-    res.json(data)
+  // res.sendFile(path.join(__dirname, '/db.json'));
+  let notes = parse();
+  res.json(notes);
 });
 
+let id = 1;
 // Make new notes that takes json
 app.post('/api/notes', (req, res) => {
-    const note = (re)
-})
+    fs.readFile('/db.json', 'utf8', (err, notes) => {
+      if (err) throw err;
+});
+});
 
 // Start server
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`)); 
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+}); 
